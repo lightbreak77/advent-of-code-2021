6aa842c3e24c10232b537e8d15ac3e3228f50041
Light <90529969+lightbreak77@users.noreply.github.com>
2021-12-03 17:55:41 +0000

    Add Day 1

---
+++ solutions/solution_1.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+with (Path(__file__).parent / "../inputs/input-1.txt").open() as file:
+    input_measurements = [int(line) for line in file]
+
+def part_1(measurements: list[int]) -> int:
+    answer = 0
+
+    for index in range(len(measurements) - 1):
+        if measurements[index + 1] > measurements[index]:
+            answer += 1
+    
+    return answer
+
+def part_2(measurements: list[int]) -> int:
+    answer = 0
+
+    for index in range(len(measurements) - 3):
+        if sum(measurements[index + 1 : index + 4]) > sum(measurements[index : index + 3]):
+            answer += 1
+
+    return answer
+
+print(part_1(input_measurements))
+
+print(part_2(input_measurements))
