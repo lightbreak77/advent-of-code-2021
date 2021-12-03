e9b9393fd316cfaf170b33c3482276a964e60068
Light <90529969+lightbreak77@users.noreply.github.com>
2021-12-03 18:38:58 +0000

    Add Day 1 type hints

--- solutions/solution_1.py
+++ solutions/solution_1.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 
 with (Path(__file__).parent / "../inputs/input-1.txt").open() as file:
-    input_measurements = [int(line) for line in file]
+    input_measurements: list = [int(line) for line in file]
 
 def part_1(measurements: list[int]) -> int:
-    answer = 0
+    answer: int = 0
 
+    index: int
     for index in range(len(measurements) - 1):
         if measurements[index + 1] > measurements[index]:
             answer += 1
@@ -13,8 +14,9 @@ def part_1(measurements: list[int]) -> int:
     return answer
 
 def part_2(measurements: list[int]) -> int:
-    answer = 0
+    answer: int = 0
 
+    index: int
     for index in range(len(measurements) - 3):
         if sum(measurements[index + 1 : index + 4]) > sum(measurements[index : index + 3]):
             answer += 1
